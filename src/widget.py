947536c2e836f32b0cd925a88c5e46ec73336540
Горшков Алексей <lego.77@icloud.com>
2025-06-29 20:18:26 +0300

    добавлена функция mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,16 @@
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
+
+
+def mask_account_card(account_card: str) -> str:
+    """
+    1. Принимает номер счета и возвращает его маску в формате ****************XXXX
+    2. Принимает номер карты и возвращает ее маску в формате XXXX XX** **** XXXX
+    """
+    out_account_card = "".join(letter if letter.isdigit() else "" for letter in account_card)
+    if len(out_account_card) == 16:
+        return get_mask_card_number(out_account_card)
+    elif len(out_account_card) == 20:
+        return get_mask_account(out_account_card)
+    else:
+        return "Ошибка чтения параметров"
