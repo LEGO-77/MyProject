01d573ba91dcbaccd4d32b6705c34082c13c353d
Горшков Алексей <lego.77@icloud.com>
2025-07-07 23:25:53 +0400

    add function filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,11 @@
-def filter_by_state(dict_list : list, key: str = "EXECUTED") -> list:
+def filter_by_state(dict_list: list, key: str = "EXECUTED") -> list:
     new_dict = []
     for dict in dict_list:
         if dict["state"] == key:
             new_dict.append(dict)
     return new_dict
 
-def sort_by_date(dict_list : list, sort_by: str) -> list:
-    pass
+
+def sort_by_date(dict_list: list, sort_desc: bool = True) -> list:
+    new_dict = sorted(dict_list, key=lambda dict: dict["date"], reverse=sort_desc)
+    return new_dict
