c38e5a89c0fae783d47f958f769f9c19891fe548
Горшков Алексей <lego.77@icloud.com>
2025-07-07 22:34:41 +0400

    add processing module

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.widget import get_date
 from src.widget import mask_account_card
+from src.processing import filter_by_state
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Счет 73654108430135874305"))
 print(get_date("2024-03-11T02:26:18.671407"))
+print(filter_by_state(["1", "2"], "state"))
